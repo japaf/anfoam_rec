e443ad7090e0cedcf63b788148368d96cf9dc6e1
Jiri Kolar <jiri1kolar@gmail.com>
2017-05-08 15:18:03 +0200

    update

--- par_study_strutpor.py
+++ par_study_strutpor.py
@@ -16,28 +16,30 @@ import foam_relax
 __author__ = 'jiri1kolar'
 __email__ = "jiri1kolar@gmail.com"
 
-por_range = [0.92,0.93,0.94,0.95,0.96,0.97]
-strut_range = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-
+#por_range = [0.92,0.93,0.94,0.95,0.96,0.97]
+#strut_range = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+por_range=[0.92,0.93]
+strut_range=[0.3]
 
 def main():
     common.init_logging()
     logging.info("Running parametric study for various porosity and strut content:")
     print('Porosity range:', por_range)
     print('Strut range:', strut_range)
-    outstream = open(args.output_file, 'a')
-    outstream.write('#Porosity strut-content resolution\n')
-    for porosity in por_range:
-        for strut in strut_range:
-            relax_config = {"porosity": porosity, "strut-content": strut}
-            relax_config['relax-cmd'] = args.relax_cmd_file
-            relax_files = foam_relax.init_file_option(args.input_file + '_')
-            relax_config.update(relax_files)
-            foam_relax.optimize_strut_content(relax_config['relaxed-dry'], relax_config)
-            [final_porosity, final_resolution] = foam_relax.relax_porosity(relax_config)
-            if os.path.isfile(relax_config['vox-out']):
-                os.remove(relax_config['vox-out'])
-            outstream.write('{0:f}, {1:f}, {2:d}\n'.format(final_porosity, strut, final_resolution))
+    with open(args.output_file, 'a') as outstream:
+        outstream.write('#Porosity strut-content resolution\n')
+        for porosity in por_range:
+            for strut in strut_range:
+                relax_config = {"porosity": porosity, "strut-content": strut}
+                relax_config['relax-cmd'] = args.relax_cmd_file
+                relax_files = foam_relax.init_file_option(args.input_file + '_')
+                relax_config.update(relax_files)
+                foam_relax.optimize_strut_content(relax_config['relaxed-dry'], relax_config)
+                [final_porosity, final_resolution] = foam_relax.relax_porosity(relax_config)
+                if os.path.isfile(relax_config['vox-out']):
+                    os.remove(relax_config['vox-out'].__str__())
+                outstream.write('{0:f}, {1:f}, {2:d}\n'.format(final_porosity, strut, final_resolution))
+                outstream.flush()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
